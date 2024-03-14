2dd78fb83cecc3e8f67374decb6ca3e0af1ea264
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-03-14 15:29:51 +0100

    in commit

---
+++ src/main/java/marco/entities/Order.java
@@ -0,0 +1,62 @@
+package marco.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+
+    private long id;
+    private String status;
+    private LocalDate orderDate;
+
+    private LocalDate deliveryDate;
+
+    private List<Product> products;
+    private Customer customer;
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                ", orderDate=" + orderDate +
+                ", deliveryDate=" + deliveryDate +
+                ", products=" + products +
+                ", customer=" + customer +
+                '}';
+    }
+
+    public Order(long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+}
+
